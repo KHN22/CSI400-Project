1df2dabae14b39dc5d4f5a8bcf63f5efd5ee8c02
JBxTNz <worathep.submit_work@hotmail.com>
2025-11-09 00:04:51 +0700

    add schema

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -4,7 +4,8 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true, lowercase: true, trim: true },
   passwordHash: { type: String, required: true },
   username: { type: String },
-  role: { type: String, enum: ['Guest', 'Admin'], default: 'Guest' } // new
+  profileImage: { type: String, default: null }, // NEW: stores profile picture path or URL
+  role: { type: String, enum: ['Guest', 'Admin'], default: 'Guest' }// new
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
