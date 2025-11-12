d4e7720666d779d5ee93e62bf1833e43a4a38f2a
KHN22 <khingza002@gmail.com>
2025-11-13 00:12:39 +0700

    AllowOrigin

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 // อนุญาต Domains ที่กำหนด
 const allowedOrigins = [
-  'https://csi400-project.vercel.app', // Frontend Domain
+  'https://csi-400-project-git-main-khingza002-gmailcoms-projects.vercel.app', // Frontend Domain
   'http://localhost:3000', // สำหรับการพัฒนาใน Local
 ];
 
